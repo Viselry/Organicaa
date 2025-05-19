50450d18cba0a7a4bacea0c058089662173c4cf8
Viselry <khanhbarton@gmail.com>
2025-05-19 10:47:47 +0700

    build again

--- Server/src/test/java/com/organica/OrganicaApplicationTests.java
+++ Server/src/test/java/com/organica/OrganicaApplicationTests.java
@@ -1,9 +1,12 @@
 package com.organica;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = KafkaAutoConfiguration.class)
 class OrganicaApplicationTests {
 
 	@Test
