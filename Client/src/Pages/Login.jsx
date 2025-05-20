9b04cd2e0d6aa359673a660820036b4719ee53f0
Djuybu <109862020+Djuybu@users.noreply.github.com>
2025-05-20 22:46:29 +0700

    Update Login.jsx

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -180,7 +180,7 @@ export const Login = () => {
   >
     <p style={{ marginBottom: 0 }}>
       Don't you have an account?
-      <a id="register-link" href="signup">
+      <a id="register-link" href="/signup">
         Sign Up!
       </a>
     </p>
